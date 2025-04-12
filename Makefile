400de8f19417bafb9a002d769a5e08211d9906ef
Ky Nguyen <cuty0406@outlook.com>
2025-04-12 14:00:00 +0700

    Fix makefile and docker for new version

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 start: build
 	@docker compose up --build
-
 build:
-	@docker-compose rm -v --force --stop
-	@docker rmi ticket-booking-backend
-
+	-@docker compose rm -v --force --stop
+	-@docker rmi ticket-booking-backend
+	@docker compose build
--- db/database.go
+++ db/database.go
@@ -10,24 +10,24 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-func Init(config *config.EnvConfig, DBMigrator func(db *gorm.DB) error) *gorm.DB {
+func Init(config *config.EnvConfig, DBMigrator func(*gorm.DB) error) *gorm.DB {
 	uri := fmt.Sprintf(`
-		host=%s user=%s dbname=%s password=%s sslmode=%s port=5432
-
-	`, config.DBHost, config.DBUser, config.DBName, config.DBPassword, config.DBSSLMode)
+		host=%s user=%s dbname=%s password=%s sslmode=%s port=5432`,
+		config.DBHost, config.DBUser, config.DBName, config.DBPassword, config.DBSSLMode,
+	)
 
 	db, err := gorm.Open(postgres.Open(uri), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 
 	if err != nil {
-		log.Fatalf("Unable to connect to the database: %e", err)
+		log.Fatalf("Unable to connect to database: %v", err)
 	}
 
-	log.Info("Connected to the database!")
+	log.Info("Connected to the database")
 
 	if err := DBMigrator(db); err != nil {
-		log.Fatalf("Unable to migrate tables: %e", err)
+		log.Fatalf("Unable to migrate: %v", err)
 	}
 
 	return db
