e09b9e264991d83f98b5a7e95ea280b94b3dbd2d
Ky Nguyen <cuty0406@outlook.com>
2025-04-23 23:46:45 +0700

    Create handlers for login and register

--- go.mod
+++ go.mod
@@ -7,14 +7,20 @@ require (
 	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/joho/godotenv v1.5.1
-	golang.org/x/crypto v0.17.0
+	golang.org/x/crypto v0.33.0
 )
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.26.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect
 )
 
@@ -31,7 +37,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
--- services/auth.go
+++ services/auth.go
@@ -10,6 +10,7 @@ import (
 	"github.com/TechmoNoway/golang-ticket-booking-backend/models"
 	"github.com/TechmoNoway/golang-ticket-booking-backend/utils"
 	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -47,7 +48,40 @@ func (s *AuthService) Login(ctx context.Context, loginData *models.AuthCredentia
 }
 
 func (s *AuthService) Register(ctx context.Context, registerData *models.AuthCredentials) (string, *models.User, error) {
-	return "", nil, nil
+	if !models.IsValidEmail(registerData.Email) {
+		return "", nil, fmt.Errorf("please, provide a valid email to register")
+	}
+
+	if _, err := s.repository.GetUser(ctx, "email = ?", registerData.Email); !errors.Is(err, gorm.ErrRecordNotFound) {
+		return "", nil, fmt.Errorf("the user email is already in use")
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(registerData.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", nil, err
+	}
+
+	registerData.Password = string(hashedPassword)
+
+	user, err := s.repository.RegisterUser(ctx, registerData)
+
+	if err != nil {
+		return "", nil, err
+	}
+
+	claims := jwt.MapClaims{
+		"id":   user.ID,
+		"role": user.Role,
+		"exp":  time.Now().Add(time.Hour * 168).Unix(),
+	}
+
+	token, err := utils.GenerateJWT(claims, jwt.SigningMethodHS256, os.Getenv("JWT_SECRET"))
+
+	if err != nil {
+		return "", nil, err
+	}
+
+	return token, user, nil
 }
 
 func NewAuthService(repository models.AuthRepository) models.AuthService {
